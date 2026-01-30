cfe1467b4fa1b7f3702d658b879e3b2af5df80b4
Hiếu Huỳnh <mike.beplus@gmail.com>
2026-01-31 00:09:58 +0700

    First commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config = {
+  logo: <span>Optstack</span>,
+  project: {
+    link: 'https://github.com/miketropi/optstack',
+  },
+  chat: {
+    link: 'https://#',
+  },
+  docsRepositoryBase: 'https://github.com/miketropi/optstack',
+  footer: {
+    text: 'Optstack documentation',
+  },
+}
+
+export default config
