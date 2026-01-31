42c789462284aa96715e49a780d6c7a7ce525e06
Hiếu Huỳnh <mike.beplus@gmail.com>
2026-01-31 23:10:41 +0700

    Update post & add feature i18n en-US + vi-VN

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,26 @@ import React from 'react'
 // import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Optstack'
+    }
+  },
+  banner: {
+    key: 'optstack-0.1-release',
+    text: (
+      <a href="https://github.com/miketropi/optstack" target="_blank">
+        🎉 Optstack 0.1 is released. Read more →
+      </a>
+    )
+  },
   logo: <span>Optstack</span>,
   project: {
     link: 'https://github.com/miketropi/optstack',
   },
-  chat: {
-    link: 'https://#',
-  },
+  // chat: {
+  //   link: 'https://#',
+  // },
   docsRepositoryBase: 'https://github.com/miketropi/optstack',
   footer: {
     text: 'Optstack documentation',
